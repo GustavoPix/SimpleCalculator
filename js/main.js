d134f947a4fb92487878fbce02fb826b1a064acf
Gustavo Carvalho <gustavo1net@hotmail.com>
2020-04-18 19:33:28 -0300

    sum function

--- js/main.js
+++ js/main.js
@@ -1,10 +1,48 @@
 const vm_main = new Vue({
     el:"#main",
     data:{
-        
+        history:"",
+        atual:"",
+        result:0,
+        lastOperator:""
     },
     methods: {
-        
+        addNumber(x)
+        {
+            if(this.atual.length < 8)
+            {
+                this.atual += "" + x;
+            }
+        },
+        addOperator(o)
+        {
+            if(this.lastOperator != "")
+            {
+                switch(this.lastOperator)
+                {
+                    case "+":
+                        this.result += parseFloat(this.atual)
+                        break;
+                    case "-":
+                        this.result -= this.atual
+                        break;
+                    case "*":
+                        this.result *= this.atual
+                        break;
+                    case "/":
+                        this.result /= this.atual
+                        break;
+                }
+            }
+            else
+            {
+                this.result = parseFloat(this.atual);
+            }
+            this.history += `${this.atual} ${o} `;
+            //this.result = ParseFloat(this.atual);
+            this.lastOperator = o;
+            this.atual = ""
+        }
     },
     watch: {
         
