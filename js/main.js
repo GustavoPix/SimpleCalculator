1928c542682d36e6c6c18347a7255b307fa63645
Gustavo Carvalho <gustavo1net@hotmail.com>
2020-04-23 18:14:58 -0300

    last operators

--- js/main.js
+++ js/main.js
@@ -4,11 +4,16 @@ const vm_main = new Vue({
         history:"",
         atual:"",
         result:0,
-        lastOperator:""
+        lastOperator:"",
+        nextReset:false
     },
     methods: {
         addNumber(x)
         {
+            if(this.nextReset)
+            {
+                this.clearAll();
+            }
             if(this.atual.length < 8)
             {
                 this.atual += "" + x;
@@ -16,7 +21,58 @@ const vm_main = new Vue({
         },
         addOperator(o)
         {
+            if(this.nextReset)
+            {
+                this.clearAll();
+            }
             if(this.lastOperator != "")
+            {
+                if(this.atual != "")
+                {
+                    switch(this.lastOperator)
+                    {
+                        case "+":
+                            this.result += parseFloat(this.atual)
+                            break;
+                        case "-":
+                            this.result -= parseFloat(this.atual)
+                            break;
+                        case "*":
+                            this.result *= parseFloat(this.atual)
+                            break;
+                        case "/":
+                            this.result /= parseFloat(this.atual)
+                            break;
+                    }
+                    if(this.result.toString().length > 8)
+                    {
+                        this.result = "ERR";
+                        this.nextReset = true;
+                    }
+                }
+                else
+                {
+                    this.history = `${this.history.slice(0,this.history.length-2)}`;
+                }
+            }
+            else
+            {
+                if(this.atual != "")
+                {
+                    this.result = parseFloat(this.atual);
+                }
+            }
+            if(this.atual != "" || this.lastOperator != "")
+            {
+                this.history += `${this.atual} ${o} `;
+                //this.result = ParseFloat(this.atual);
+                this.lastOperator = o;
+                this.atual = "";
+            }
+        },
+        resultOperation()
+        {
+            if(this.atual != "")
             {
                 switch(this.lastOperator)
                 {
@@ -24,24 +80,61 @@ const vm_main = new Vue({
                         this.result += parseFloat(this.atual)
                         break;
                     case "-":
-                        this.result -= this.atual
+                        this.result -= parseFloat(this.atual)
                         break;
                     case "*":
-                        this.result *= this.atual
+                        this.result *= parseFloat(this.atual)
                         break;
                     case "/":
-                        this.result /= this.atual
+                        this.result /= parseFloat(this.atual)
                         break;
+                    default:
+                        this.result = parseFloat(this.atual)
+                }
+                if(this.result.toString().length > 8)
+                {
+                    this.result = "ERR";
                 }
             }
             else
             {
-                this.result = parseFloat(this.atual);
+                this.history = `${this.history.slice(0,this.history.length-2)}`;
             }
-            this.history += `${this.atual} ${o} `;
+            this.history += `${this.atual} = `;
             //this.result = ParseFloat(this.atual);
-            this.lastOperator = o;
-            this.atual = ""
+            //this.lastOperator = o;
+            this.atual = "";
+            this.nextReset = true;
+        },
+        invertAtual()
+        {
+            if(this.atual != "")
+            {
+                this.atual = (parseFloat(this.atual) * -1).toString(); 
+            }
+        },
+        deleteLastNumber()
+        {
+            this.atual = this.atual.slice(0,this.atual.length - 1);
+        },
+        addDot()
+        {
+            if(!this.atual.includes('.'))
+            {
+                this.addNumber('.');
+            }
+        },
+        clearAll()
+        {
+            this.history = "";
+            this.atual = "";
+            this.result = 0;
+            this.lastOperator = "";
+            this.nextReset = false;
+        },
+        clear()
+        {
+            this.atual = "";
         }
     },
     watch: {
